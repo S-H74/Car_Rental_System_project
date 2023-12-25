2685069e12f6a44a030d4de7185f30d9901fd18f
Sherif <Sherif-74@gmail.com>
2023-12-25 23:56:05 +0200

    File Car.cpp

---
+++ Car.cpp
@@ -0,0 +1,82 @@
+#include "Car.h"
+
+void Car::set_car_make(string make1)
+{
+	car_make = make1;
+}
+
+string Car::get_car_make()
+{
+	return car_make;
+}
+
+void Car::set_car_model(string model1)
+{
+	car_model = model1;
+}
+
+string Car::get_car_model()
+{
+	return car_model;
+}
+
+void Car::set_car_catig(string catig)
+{
+	car_catig = catig;
+
+}
+
+string Car::get_car_catig()
+{
+	return car_catig;
+}
+
+void Car::set_car_year(int year)
+{
+	car_year = year;
+}
+
+int Car::get_car_year()
+{
+	return car_year;
+}
+
+void Car::set_car_trans(char trans)
+{
+	car_trans = trans;
+}
+
+char Car::get_car_trans()
+{
+	return car_trans;
+}
+
+void Car::set_car_passenger(int pass)
+{
+	car_passnger = pass;
+}
+
+int Car::get_car_passenger()
+{
+	return car_passnger;
+}
+
+void Car::set_car_price(float price)
+{
+	car_price = price;
+}
+
+float Car::get_car_price()
+{
+	return car_price;
+}
+
+void Car::set_car_info(string WD)
+{
+	car_info = WD;
+}
+
+string Car::get_car_info()
+{
+	return car_info;
+}
